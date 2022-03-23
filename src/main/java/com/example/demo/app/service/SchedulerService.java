ffa663bc2ab89763925581a7ee7736988cbbd5c3
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-03-23 08:22:38 +0200

    Test @Scheduled

--- src/main/java/com/example/demo/app/service/SchedulerService.java
+++ src/main/java/com/example/demo/app/service/SchedulerService.java
@@ -17,7 +17,7 @@ public class SchedulerService {
 
     // todo source of data - it's app context ot external store
     //  so here not need @RequestScope
-    @Scheduled(cron="0 0 * * * *")
+//    @Scheduled(fixedRate = 5000)
     public void greetings() {
        log.info(employeeDetails.getId()); // accessing request scope bean
     }

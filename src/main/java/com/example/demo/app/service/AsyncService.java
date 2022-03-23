73aee9798ac4f5438f26b23785d3088a18451fd7
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-03-23 09:00:00 +0200

    Flip service method

--- src/main/java/com/example/demo/app/service/AsyncService.java
+++ src/main/java/com/example/demo/app/service/AsyncService.java
@@ -1,11 +1,8 @@
 package com.example.demo.app.service;
 
 import com.example.demo.app.dto.EmployeeDetailsDto;
-import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.Nullable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -13,24 +10,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class AsyncService {
 
-    @Async
-    public void getAsyncServerNameAndRequestQuery(EmployeeDetailsDto employeeDetails, HttpServletRequest httpServletRequest) {
-        getServerNameAndRequestQuery(employeeDetails, httpServletRequest);
+    private final SimpleService simpleService;
+
+    public AsyncService(SimpleService simpleService) {
+        this.simpleService = simpleService;
     }
 
-    public String getServerNameAndRequestQuery(@NotNull EmployeeDetailsDto employeeDetails,
-                                               @Nullable HttpServletRequest httpServletRequest) {
-        log.info("User uuid = {}", employeeDetails.getId());
-
-        String requestQueryString = Optional.ofNullable(httpServletRequest)
-            .map(HttpServletRequest::getQueryString)
-            .orElse(null);
-        log.info("Request query: {}", requestQueryString);
-
-        String serverName = Optional.ofNullable(httpServletRequest)
-            .map(HttpServletRequest::getServerName)
-            .orElse(null);
-        return serverName + " " + requestQueryString;
+    @Async
+    public void getAsyncServerNameAndRequestQuery(EmployeeDetailsDto employeeDetails, HttpServletRequest httpServletRequest) {
+        simpleService.getServerNameAndRequestQuery(employeeDetails, httpServletRequest);
     }
 
 }
--- src/main/java/com/example/demo/app/service/SimpleService.java
+++ src/main/java/com/example/demo/app/service/SimpleService.java
@@ -1,27 +1,43 @@
 package com.example.demo.app.service;
 
 import com.example.demo.app.dto.EmployeeDetails;
+import com.example.demo.app.dto.EmployeeDetailsDto;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class SimpleService {
 
-    private final AsyncService asyncService;
     private final EmployeeDetails employeeDetails;
     private final HttpServletRequest httpServletRequest;
 
-    public SimpleService(AsyncService asyncService, EmployeeDetails employeeDetails,
+    public SimpleService(EmployeeDetails employeeDetails,
                          HttpServletRequest httpServletRequest) {
-        this.asyncService = asyncService;
         this.employeeDetails = employeeDetails;
         this.httpServletRequest = httpServletRequest;
     }
 
     public String getServerNameAndRequestQuery() {
-        return asyncService.getServerNameAndRequestQuery(employeeDetails.getEmployeeDetailsDto(), httpServletRequest);
+        return getServerNameAndRequestQuery(employeeDetails.getEmployeeDetailsDto(), httpServletRequest);
     }
 
+    public String getServerNameAndRequestQuery(@NotNull EmployeeDetailsDto employeeDetails,
+                                               @Nullable HttpServletRequest httpServletRequest) {
+        log.info("User uuid = {}", employeeDetails.getId());
+
+        String requestQueryString = Optional.ofNullable(httpServletRequest)
+            .map(HttpServletRequest::getQueryString)
+            .orElse(null);
+        log.info("Request query: {}", requestQueryString);
+
+        String serverName = Optional.ofNullable(httpServletRequest)
+            .map(HttpServletRequest::getServerName)
+            .orElse(null);
+        return serverName + " " + requestQueryString;
+    }
 }
